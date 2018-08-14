39d791446dc6282c1fcd17e16c8333c2f30ea325
Ruchira Wageesha <ruchira.wageesha@gmail.com>
2018-08-14 09:37:34 +0530

    updating

--- index.js
+++ index.js
@@ -1,11 +1,29 @@
 var dust = require('dust')();
 var serand = require('serand');
 var utils = require('utils');
+var captcha = require('captcha');
 
 dust.loadSource(dust.compile(require('./template'), 'accounts-signin'));
 
 module.exports = function (sandbox, options, done) {
-    dust.render('accounts-signin', {}, function (err, out) {
+    var home = options.location || '/';
+    var signup = 'accounts://signup';
+    var suffix = '';
+    var append = function (suff) {
+        suffix += (suffix ? '&' : '?') + suff;
+    }
+    if (options.clientId) {
+        append('client_id=' + options.clientId);
+    }
+    if (options.location) {
+        append('redirect_uri=' + options.location);
+    }
+    signup += suffix;
+    signup = utils.resolve(signup);
+
+    var captchaId;
+
+    dust.render('accounts-signin', {home: home, signup: signup}, function (err, out) {
         if (err) {
             return done(err);
         }
@@ -14,7 +32,15 @@ module.exports = function (sandbox, options, done) {
             var el = $('.accounts-signin', sandbox);
             var username = $('.username', el).val();
             var password = $('.password', el).val();
-            authenticate(username, password, options);
+            captcha.response(captchaId, function (err, captcha) {
+                if (err) {
+                    return console.error(err);
+                }
+                if (!captcha) {
+                    return;
+                }
+                authenticate(captcha, username, password, options);
+            });
             return false;
         });
         sandbox.on('click', '.accounts-signin .facebook', function (e) {
@@ -22,13 +48,27 @@ module.exports = function (sandbox, options, done) {
             serand.emit('user', 'oauth', options);
             return false;
         });
-        done(null, function () {
-            $('.accounts-signin', sandbox).remove();
+        done(null, {
+            clean: function () {
+                $('.accounts-signin', sandbox).remove();
+            },
+            ready: function () {
+                captcha.render($('.captcha', sandbox), {
+                    success: function () {
+                        $('.accounts-signin .signin', sandbox).removeAttr('disabled');
+                    }
+                }, function (err, id) {
+                    if (err) {
+                        return console.error(err);
+                    }
+                    captchaId = id;
+                });
+            }
         });
     });
 };
 
-var authenticate = function (username, password, options) {
+var authenticate = function (captcha, username, password, options) {
     $.ajax({
         method: 'POST',
         url: utils.resolve('accounts://apis/v/tokens'),
@@ -37,6 +77,9 @@ var authenticate = function (username, password, options) {
             grant_type: 'password',
             username: username,
             password: password
+        },
+        headers: {
+            'X-Captcha': captcha
         },
         contentType: 'application/x-www-form-urlencoded',
         dataType: 'json',
