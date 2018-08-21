b5e8c99a0325be2fa68df8b64beaef45ddb44473
Ruchira Wageesha <ruchira.wageesha@gmail.com>
2018-08-21 09:29:32 +0530

    updating

--- index.js
+++ index.js
@@ -2,8 +2,42 @@ var dust = require('dust')();
 var serand = require('serand');
 var utils = require('utils');
 var captcha = require('captcha');
+var form = require('form');
 
 dust.loadSource(dust.compile(require('./template'), 'accounts-signin'));
+
+
+var configs = {
+    username: {
+        find: function (context, source, done) {
+            var value = $('input', source).val();
+            if (!value) {
+                return done(null, 'Please enter your username');
+            }
+            if (!is.email(value)) {
+                return done(null, 'Please enter a valid email address');
+            }
+            done(null, null, value);
+        },
+        update: function (context, source, error, value, done) {
+            $('input', source).val(value);
+            done()
+        }
+    },
+    password: {
+        find: function (context, source, done) {
+            var value = $('input', source).val();
+            if (!value) {
+                return done(null, 'Please enter your password');
+            }
+            done(null, null, value);
+        },
+        update: function (context, source, error, value, done) {
+            $('input', source).val(value);
+            done();
+        }
+    },
+};
 
 module.exports = function (sandbox, options, done) {
     var home = options.location || '/';
@@ -27,43 +61,68 @@ module.exports = function (sandbox, options, done) {
         if (err) {
             return done(err);
         }
-        sandbox.append(out);
-        sandbox.on('click', '.accounts-signin .signin', function (e) {
-            var el = $('.accounts-signin', sandbox);
-            var username = $('.username', el).val();
-            var password = $('.password', el).val();
-            captcha.response(captchaId, function (err, captcha) {
-                if (err) {
-                    return console.error(err);
-                }
-                if (!captcha) {
-                    return;
-                }
-                authenticate(captcha, username, password, options);
-            });
-            return false;
-        });
-        sandbox.on('click', '.accounts-signin .facebook', function (e) {
-            options.type = 'facebook';
-            serand.emit('user', 'oauth', options);
-            return false;
-        });
-        done(null, {
-            clean: function () {
-                $('.accounts-signin', sandbox).remove();
-            },
-            ready: function () {
-                captcha.render($('.captcha', sandbox), {
-                    success: function () {
-                        $('.accounts-signin .signin', sandbox).removeAttr('disabled');
-                    }
-                }, function (err, id) {
+        var elem = sandbox.append(out);
+        var lform = form.create(elem, configs);
+        lform.render({}, function (err) {
+            if (err) {
+                return done(err);
+            }
+            var signin = $('.accounts-signin .signin', elem);
+            sandbox.on('click', '.accounts-signin .signin', function (e) {
+                lform.find(function (err, errors, data) {
                     if (err) {
                         return console.error(err);
                     }
-                    captchaId = id;
+                    if (errors) {
+                        lform.update(errors, data, function (err) {
+                            if (err) {
+                                return console.error(err);
+                            }
+                            signin.removeAttr('disabled');
+                        });
+                        return;
+                    }
+                    lform.update(errors, data, function (err) {
+                        if (err) {
+                            return console.error(err);
+                        }
+                        lform.create(data, function (err, data) {
+                            captcha.response(captchaId, function (err, captcha) {
+                                if (err) {
+                                    return console.error(err);
+                                }
+                                if (!captcha) {
+                                    return;
+                                }
+                                authenticate(captcha, data.username, data.password, options);
+                            });
+                        });
+                    });
                 });
-            }
+                return false;
+            });
+            sandbox.on('click', '.accounts-signin .facebook', function (e) {
+                options.type = 'facebook';
+                serand.emit('user', 'oauth', options);
+                return false;
+            });
+            done(null, {
+                clean: function () {
+                    $('.accounts-signin', sandbox).remove();
+                },
+                ready: function () {
+                    captcha.render($('.captcha', sandbox), {
+                        success: function () {
+                            $('.accounts-signin .signin', sandbox).removeAttr('disabled');
+                        }
+                    }, function (err, id) {
+                        if (err) {
+                            return console.error(err);
+                        }
+                        captchaId = id;
+                    });
+                }
+            });
         });
     });
 };
